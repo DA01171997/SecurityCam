67801bd54a0c2c182d68d8be03f7aeb1a1279090
Cristopher Hernandez <DoughboysWWI@gmail.com>
2019-05-02 18:48:11 -0700

    Switch camera logic to always show video, then check distance

--- monitor.py
+++ monitor.py
@@ -20,7 +20,6 @@ class SecurityCamera:
             from_email -> email address to use for sending notifications
             email_password -> password to use for sending notifications from from_email
         trigger_dist -> distance in CM which is the threshold for triggering the camera
-        scan_interval -> time to wait (in seconds) between checking sensors
         trigger_interval -> time to wait (in seconds) between triggering detection again
 
     other attributes:
@@ -29,11 +28,10 @@ class SecurityCamera:
         body_cascade -> computer vision classifier for bodies
         frame_data -> holds information on any given frame the camera may be analyzing
     """
-    def __init__(self, to_email, from_email, email_password, trigger_dist=100, scan_interval=5, trigger_interval=15):
+    def __init__(self, to_email, from_email, email_password, trigger_dist=100, trigger_interval=15):
         self.mailer = Mail(from_email, email_password)  # mailer object
         self.to_email = to_email    # destination email
         self.trigger_dist = trigger_dist    # distance threshold
-        self.scan_interval = scan_interval  # scan interval
         self.last_trigger = datetime.now()  # last trigger timestamp
         self.trigger_interval = trigger_interval    # trigger cooldown
         self.range_finder = RangeFinder()   # range finder object
@@ -87,45 +85,45 @@ class SecurityCamera:
 
         try:
             print('Monitoring...')
-            while True:
-                distance = self.range_finder.get_distance() # check the distance sensor
-                if distance < self.trigger_dist and self.is_trigger_ready():    # no cooldown, and target is close
-                    print('Distance threshold tripped!')
-                    self.last_trigger = datetime.now()
-                    with PiCamera() as camera:  # open the camera
-                        raw_capture = PiRGBArray(camera, size=(640, 480))   # configure the camera
-                        camera.resolution = (640, 480)
-                        camera.framerate = 60
-                        time.sleep(0.1)  # allow camera to warm up for split second
-                        # begin capturing frames from the camera
-                        for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
-                            self.frame_data['img'] = frame.array    # get the image data
-                            self.frame_data['img-gray'] = cv2.cvtColor(self.frame_data['img'], cv2.COLOR_BGR2GRAY)  # convert image data to gray
-                            # run computer vision
-                            self.frame_data['faces'] = self.face_cascade.detectMultiScale(self.frame_data['img-gray'], 1.3, 5)
-                            self.frame_data['bodies'] = self.body_cascade.detectMultiScale(self.frame_data['img-gray'], 1.3, 5)
+            with PiCamera() as camera:  # open the camera
+                raw_capture = PiRGBArray(camera, size=(640, 480))   # configure the camera
+                camera.resolution = (640, 480)
+                camera.framerate = 60
+                time.sleep(0.1)  # allow camera to warm up for split second
+                # begin capturing frames from the camera
+                for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+                    self.frame_data['img'] = frame.array    # get the image data
+                    self.frame_data['img-gray'] = cv2.cvtColor(self.frame_data['img'], cv2.COLOR_BGR2GRAY)  # convert image data to gray
+                    # run computer vision
+                    self.frame_data['faces'] = self.face_cascade.detectMultiScale(self.frame_data['img-gray'], 1.3, 5)
+                    self.frame_data['bodies'] = self.body_cascade.detectMultiScale(self.frame_data['img-gray'], 1.3, 5)
 
-                            # outline all detections in the image
-                            for (x_top, y_top, width, height) in self.frame_data['bodies']:
-                                cv2.rectangle(self.frame_data['img'], (x_top, y_top), (x_top + width, y_top + height), (255, 0, 0), 2)
-                            for (x_top, y_top, width, height) in self.frame_data['faces']: 
-                                cv2.rectangle(self.frame_data['img'], (x_top, y_top), (x_top + width, y_top + height), (255, 255, 0), 2)
+                    # outline all detections in the image
+                    for (x_top, y_top, width, height) in self.frame_data['bodies']:
+                        cv2.rectangle(self.frame_data['img'], (x_top, y_top), (x_top + width, y_top + height), (255, 0, 0), 2)
+                    for (x_top, y_top, width, height) in self.frame_data['faces']: 
+                        cv2.rectangle(self.frame_data['img'], (x_top, y_top), (x_top + width, y_top + height), (255, 255, 0), 2)
 
-                            # if detections were found, send th
-                            if len(self.frame_data['faces']) > 0 or len(self.frame_data['bodies']) > 0:
-                                print('Face or body detected! Sending alert...')
+                    cv2.imShow('Camera', self.frame_data['img'])
+
+                    # if detections were found, send th
+                    if (len(self.frame_data['faces']) > 0 or 
+                        len(self.frame_data['bodies']) > 0) and self.is_trigger_ready():
+                            distance = self.range_finder.get_distance() # check the distance sensor
+                            if distance < self.trigger_dist:    # no cooldown, and target is close
+                                print('Distance threshold tripped!')
+                                self.last_trigger = datetime.now()
+                                print('Close proximity, Sending alert...')
                                 capture_timestamp = datetime.now()  # timestamp for capture
-                                capture_file = './detections/detection_' + str(capture_timestamp).replace(' ', '_') + '.png' # generate filename from timestamp
+                                capture_file = './detections/detection_' + \
+                                                str(capture_timestamp).replace(' ', '_') + \
+                                                '.png' # generate filename from timestamp
                                 cv2.imwrite(    # write the image data to a file
                                     capture_file,
                                     self.frame_data['img']
                                 )
                                 self.send_alert(capture_file, capture_timestamp, distance)  # send the email notice
-                                break
-                            elif self.is_trigger_ready():  # if the trigger is ready again, then stop looking for the time being
-                                break
-                            raw_capture.truncate(0) # reduce the capture data
-                time.sleep(self.scan_interval)  # wait for the scan interval
+                    raw_capture.truncate(0) # reduce the capture data
         except KeyboardInterrupt:
             print('Stopping monitoring process.')
 
@@ -170,7 +168,6 @@ if __name__ == '__main__':
         from_email=args.sender,
         email_password=args.password,
         trigger_dist=args.threshold or 100,
-        scan_interval=args.scan or 5,
         trigger_interval=args.cooldown or 15,
     )
     security_cam.monitor()
